1e003635dee3051087455898b596b839709c90cd
shaikmohammadrafi77 <rafi630297@gmail.com>
2025-12-23 10:18:20 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
 import { useState } from "react";
-import "./form.css"
+import "./Calculator.css"
 function App() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleLogin = () => {
-    alert("Username: " + username + "\nPassword: " + password);
-  };
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+  const [r, setR] = useState(0);
 
   return (
     <div>
-      <h3>Login Form</h3>
+      <h3>Simple Calculator</h3>
 
-      <input type="text" placeholder="Enter Username" onChange={e => setUsername(e.target.value)}/>
+      <input type="number" onChange={e => setA(+e.target.value)} />
+      <input type="number" onChange={e => setB(+e.target.value)} />
+
       <br /><br />
 
-      <input
-        type="password"
-        placeholder="Enter Password"
-        onChange={e => setPassword(e.target.value)}
-      />
-      <br /><br />
+      <button onClick={() => setR(a + b)}>+</button>
+      <button onClick={() => setR(a - b)}>-</button>
+      <button onClick={() => setR(a * b)}>*</button>
+      <button onClick={() => setR(a / b)}>/</button>
 
-      <button onClick={handleLogin}>Login</button>
+      <h3>Result: {r}</h3>
     </div>
   );
 }
